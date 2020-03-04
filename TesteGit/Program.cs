c62af5846387993f0ae1a3c2c32f94e26946d082
Wesley Oliveira <wesoliveirasilva2@gmail.com>
2020-03-04 18:32:59 -0300

    Acrescentada a pergunta sobre como voce esta

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -5,6 +5,7 @@ namespace TesteGit {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje? ");
         }
     }
 }
