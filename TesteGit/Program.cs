6d0c4dec8e20aea77a27f71e4e0a1d0453ebca8e
Wesley Oliveira <wesoliveirasilva2@gmail.com>
2020-03-05 16:35:35 -0300

    Acrescentando tracos

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -3,10 +3,12 @@
 namespace TesteGit {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("------------------------------");
         }
     }
 }
